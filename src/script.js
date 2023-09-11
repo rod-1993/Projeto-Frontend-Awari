9372f84902219bcc3c4a542415085803afcaf5c4
rod-1993 <brub.z@hotmail.com>
2023-09-11 18:52:49 -0300

    Atualização 11-09-2023

--- src/script.js
+++ src/script.js
@@ -1,8 +1,18 @@
 var modal = document.getElementById('formulario');
 
-//criando função ao clicar no botão (btnReservar.onclick) ele mudará o formulario (var "modal"), trocando o display do seu estilo (style.display) de "none" para "block"
-var btnReservar = document.getElementById('reservar');
-btnReservar.onclick = function() {
+//criando funções ao clicar no botão (btnReservar1.onclick) ele mudará o formulario (var "modal"), trocando o display do seu estilo (style.display) de "none" para "block"
+var btnReservar1 = document.getElementById('reservar-prod-1');
+btnReservar1.onclick = function() {
+    modal.style.display = 'block';
+}
+
+var btnReservar2 = document.getElementById('reservar-prod-2');
+btnReservar2.onclick = function() {
+    modal.style.display = 'block';
+}
+
+var btnReservar3 = document.getElementById('reservar-prod-3');
+btnReservar3.onclick = function() {
     modal.style.display = 'block';
 }
 
@@ -19,6 +29,8 @@ btnEnviar.onclick = function() {
     modal.style.display = 'none';
 }
 
+
+//criando função ao clicar na tela, fora do formulario(modal), ele tbm se fechará
 window.onclick = function(event) {
     if(event.target == modal) {
         modal.style.display = 'none'; 
